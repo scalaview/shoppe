5d4da5bcdd33140d3a31765ffcf7c04767bc0849
benko <chailink100@gmail.com>
2015-11-17 00:35:02 +0800

    add item validate

--- app/models/shoppe/basket_item.rb
+++ app/models/shoppe/basket_item.rb
@@ -5,19 +5,57 @@ module Shoppe
 
     belongs_to :product, :class_name => "Shoppe::Product"
 
+    has_many :stock_level_adjustments, :as => :parent, :dependent => :nullify, :class_name => 'Shoppe::StockLevelAdjustment'
 
     def self.add_item(ordered_item, quantity = 1)
       raise Errors::UnorderableItem, :ordered_item => ordered_item unless ordered_item.orderable?
       transaction do
-        if existing = self.where(:product_id => ordered_item.id).first
-          existing.increase!(quantity)
+        if existing = self.where(:product => ordered_item.id).first
+          existing.increase(quantity)
           existing
         else
-          new_item = self.create(:product_id => ordered_item, :quantity => 0)
-          new_item.increase!(quantity)
+          new_item = self.create(:product => ordered_item, :quantity => 0)
+          new_item.increase(quantity)
           new_item
         end
       end
+    end
+
+    def increase(amount = 1)
+      begin
+        increase!(amount)
+      rescue Exception => e
+        logger.error e.message
+        self.errors.add(:quantity, e.message)
+        false
+      end
+    end
+
+    def increase!(amount = 1)
+      transaction do
+        self.quantity += amount
+        unless self.in_stock?
+          raise Shoppe::Errors::NotEnoughStock, :ordered_item => self.product, :requested_stock => self.quantity,
+            :message => "only #{self.product.stock} in stock, requested : #{self.quantity}, not enough"
+        end
+        self.save!
+      end
+    end
+
+    def in_stock?
+      if self.product && self.product.stock_control?
+        self.product.stock >= unallocated_stock
+      else
+        true
+      end
+    end
+
+    def unallocated_stock
+      self.quantity - allocated_stock
+    end
+
+    def allocated_stock
+      0 - self.stock_level_adjustments.sum(:adjustment)
     end
 
   end
