f8de1d6e970061977c0c50a044fffb96681d881c
hmbin <334777557@qq.com>
2015-12-08 21:24:48 +0800

    edit table

--- db/migrate/20151208083823_create_shoppe_region.rb
+++ db/migrate/20151208083823_create_shoppe_region.rb
@@ -1,8 +1,8 @@
 class CreateShoppeRegion < ActiveRecord::Migration
   def change
-    create_table :shoppe_regions do |t|
-      t.integer :code, null: false, index: true
-      t.integer :parentcode, index: true
+    create_table :shoppe_region do |t|
+      t.string :code, null: false, index: true
+      t.string :parentcode, index: true
       t.string :name, null: false
       t.integer :level, null: false, index: true
       t.timestamps
